e0e291910bcff6365b39c2c8214eb995e134ee0a
Asad Saqib <asadsaqib@fas.harvard.edu>
2013-10-30 08:23:16 -0400

    Users Profile Updates
    created v_users_profile.php
    Updates the controller c_users to sync with v_users_profile (profile
    method now includes $user_name)

--- controllers/c_users.php
+++ controllers/c_users.php
@@ -3,7 +3,7 @@ class users_controller extends base_controller {
 
     public function __construct() {
         parent::__construct();
-        echo "users_controller construct called<br><br>";
+       
     } 
 
     public function index() {
@@ -24,12 +24,12 @@ class users_controller extends base_controller {
 
     public function profile($user_name = NULL) {
 
-        if($user_name == NULL) {
-            echo "No user specified";
-        }
-        else {
-            echo "This is the profile for ".$user_name;
-        }
+        $view = View::instance('v_users_profile');
+
+        $view->user_name = $user_name;
+
+        echo $view;
+    
     }
 
 } # end of the class
